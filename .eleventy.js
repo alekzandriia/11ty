c87196191c4b31dacdaab1892002d7b13ad9bdf6
Alekzandriia <alekzoreillyz@me.com>
2023-07-23 18:01:29 -0400

    format dates with luxon

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+// Format Dates
+const { DateTime } = require("luxon")
+
 // Define location of source files & public files (See: https://www.11ty.dev/docs/config/)
 module.exports = function(eleventyConfig) {
 // Pass through additional items that are not passed by default
   eleventyConfig.addPassthroughCopy('./src/styles.css')
   eleventyConfig.addPassthroughCopy('./src/assets')
+
+  // Specify Date Format
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
 
   // Return your Object options:
   return {
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Resources
 
 [11ty.rocks](https://11ty.rocks/) - Amazing resource for all things 11ty!
+[Luxon](https://moment.github.io/luxon/#/) - A powerful, modern, and friendly wrapper for JavaScript dates and times.
