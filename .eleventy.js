e33cce975568def91b5708c0b7f7de78cc1906aa
Alekzandriia <alekzoreillyz@me.com>
2023-07-23 16:00:27 -0400

    create _includes/base.njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 // Define location of source files & public files (See: https://www.11ty.dev/docs/config/)
 module.exports = function(eleventyConfig) {
+// Pass through additional items that are not passed by default
+  eleventyConfig.addPassthroughCopy('./src/styles.css')
+  eleventyConfig.addPassthroughCopy('./src/assets')
+
   // Return your Object options:
   return {
     dir: {
