d42b52a4342f69558aeed18a64fa9546df370cef
Alekzandriia <alekzoreillyz@me.com>
2023-07-23 15:40:39 -0400

    Create Eleventy config file

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+// Define location of source files & public files (See: https://www.11ty.dev/docs/config/)
+module.exports = function(eleventyConfig) {
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  }
+};
