eb87e9c431fec659e8f0509e78637a95c8d07e69
Alekzandriia <alekzoreillyz@me.com>
2023-07-26 19:50:33 -0400

    Remove css from cache

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,16 @@
 // Cache resources
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["/", "/styles.css"])
+      return cache.addAll(["/"])
     })
   )
 })
 
 // Access cached resources
 self.addEventListener("fetch", (e) => {
+  self.skipWaiting();
   e.respondWith(
     caches.match(e.request).then(response => { //check cache for resources
       return response || fetch(e.request) // return cached resource or else perform normal network  request (cache-first)
